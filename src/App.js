0b35c5454d65d84e40bec82c6b72dd5cb8dbb47d
sharan1241 <sharan1241@gmail.com>
2023-11-20 15:13:45 +0530

    update new

--- src/App.js
+++ src/App.js
@@ -1,30 +1,60 @@
-import { Route, Routes } from "react-router-dom";
-import ForgotPassword from "./pages/Authentication/ForgotPassword";
-import ResetPassword from "./pages/Application/ResetPassword";
-import { useEffect } from "react";
-import success from "./pages/Application/success";
-import Email from "./pages/Application/Email";
-import Verify from "./pages/Application/Verify";
-import Home from "./pages/Application/Home";
+import {
+  Route,
+  Routes
+} from "react-router-dom";
+import ForgotPassword from "./ForgotPassword";
+import ResetPassword from "./ResetPassword";
+import {
+  useEffect
+} from "react";
+import success from "./success";
+import Email from "./Email";
+import Verify from "./Verify";
+import Home from "./Home";
 
 
 function App() {
   const token = localStorage.getItem("refreshToken")
-   
 
-  
-  return (
-    <div className="App">
-      <Routes>
-        {!token && <Route path="/" Component={Home}/>}
-        {!token && <Route path="/forgotpassword" Component={ForgotPassword}/>}
-        {token && <Route path="/resetpassword" Component={ResetPassword}/>}
-        {token && <Route path="/success" Component={success}/>}
-        {token && <Route path="/email" Component={Email}/>}
-        {token && <Route path="/verify/:key" Component={Verify}/>}
-      </Routes>
-    </div>
-  );
-}
 
-export default App;
+
+  return ( <
+      div className = "App" >
+      <
+      Routes > {
+        !token && < Route path = "/"
+        Component = {
+          Home
+        }
+        />} {
+          !token && < Route path = "/forgotpassword"
+          Component = {
+            ForgotPassword
+          }
+          />} {
+            token && < Route path = "/resetpassword"
+            Component = {
+              ResetPassword
+            }
+            />} {
+              token && < Route path = "/success"
+              Component = {
+                success
+              }
+              />} {
+                token && < Route path = "/email"
+                Component = {
+                  Email
+                }
+                />} {
+                  token && < Route path = "/verify/:key"
+                  Component = {
+                    Verify
+                  }
+                  />} <
+                  /Routes> <
+                  /div>
+                );
+              }
+
+              export default App;
